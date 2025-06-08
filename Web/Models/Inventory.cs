336738f56d63a416dc8f03d801382cac079032cb
namtaooo <namp1622@gmail.com>
2025-06-09 00:24:04 +0700

    zz

---
+++ Web/Models/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int inventoryId { get; set; }
+
+        [Required]
+        public int productId { get; set; }
+        public ProductModel Product { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Chưa nhập.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Quá dài.")]
+        public string Unit {  get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Bắt buộc nhập.")]
+        public decimal ReorderLevel { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ExpiryDate { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public Inventory()
+        {
+            lastUpdate = DateTime.Now;
+        }
+        public Inventory(int ProductId, int initialQuantity, int ReorderQuantity, DateTime date)
+        {
+            productId = ProductId;
+            Quantity = initialQuantity;
+            ReorderLevel = ReorderQuantity;
+            ExpiryDate = date;
+            lastUpdate = DateTime.Now;
+        }
+
+        //Khi hàng dưới 50 thì cần nhập hàng lại
+        public bool NeedRestock() => Quantity <= ReorderLevel - 50;
+    }
+}
---
+++ Web/Models/InventoryTransaction.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    namespace Entity_class
+    {
+        public enum ActionEnum
+        {
+            Import = 0,
+            Reduce = 1,
+            Return = 2,
+            Sale = 3
+        } 
+        public class InventoryTransaction
+        {
+            [Key]
+            public int TransactionId { get; set; }//TransactionId INT PRIMARY KEY IDENTITY(1,1), -- Tự tăng
+
+            [Required]
+            public int ProductId { get; set; }
+            public ProductModel Product { get; set; }
+
+            [Required]
+            public ActionEnum ActionType { get; set; }
+
+            [Required]
+            [Range(0, int.MaxValue, ErrorMessage = "Số lượng ít nhaas5 là 0.")]
+            public int QuantityChanged { get; set; }
+
+            [DataType(DataType.DateTime)]
+            public DateTime ChangedTime { get; set; } = DateTime.Now; // ngày nhập hàng hoặc xuất j đó
+
+            [StringLength(200)]
+            public string note { get; set; }// ghi chú kèm theo
+
+            public InventoryTransaction()
+            {
+                ChangedTime = DateTime.Now;
+            }
+            public InventoryTransaction(int productId, ActionEnum actionType, int quantityChanged, string note = "") : this()
+            {
+                ProductId = productId;
+                ActionType = actionType;
+                QuantityChanged = quantityChanged;
+                ChangedTime = DateTime.Now;
+                this.note = note;
+            }
+        }
+    }
+}
